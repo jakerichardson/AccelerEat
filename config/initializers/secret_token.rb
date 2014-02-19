56ce2c0994e93d96bfb81a3eecafba3759a6b88f
Jake Richardson <jakerichardson5@gmail.com>
2014-02-18 16:02:59 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AccelerEat::Application.config.secret_key_base = 'a7bd0ffcf9f08252dd53facec1fa4b75880fe6cdabffed096edb61ab082005150207a9dd06d3e6a2d482bf6f80ca8b1b427510b4fba5c2bce9efb6e632bd1b3b'
